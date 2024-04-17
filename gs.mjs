f6319f8971eb5ea72f59e1a13f15a32685e30651
Andrew Fillhart <afillhar@bard.edu>
2024-04-17 15:11:03 -0400

    Partial setup of sending information to the ChatWindow.

--- gs.mjs
+++ gs.mjs
@@ -3,6 +3,7 @@ import GSItemSheet from "./module/sheets/GSItemSheet.mjs";
 import GSActorSheet from "./module/sheets/GSActorSheet.mjs";
 import { GSActor } from "./module/documents/GSActor.mjs";
 import { preloadHandlebarsTemplates } from "./module/helpers/templates.mjs";
+import { GSItem } from "./module/documents/GSItem.mjs";
 
 Hooks.once("init", () => {
 	game.gs = { GSActor }
@@ -10,7 +11,7 @@ Hooks.once("init", () => {
 	console.log("GS | Initializing Gobline Slayer TTRPG");
 
 	CONFIG.gs = gs;
-
+	CONFIG.Item.entityClass = GSItem
 	CONFIG.Combat.initiative = {
 		formula: "2d6 + @something.here",
 		decimals: 2
--- module/documents/GSItem.mjs
+++ module/documents/GSItem.mjs
@@ -1,4 +1,11 @@
 export class GSItem extends Item {
+    chatTemplate = {
+        "weapon": "systems/gs/templates/chatWindow/weapon-card.hbs",
+        "armor": "systems/gs/templates/chatWindow/armor-card.hbs",
+        "item": "systems/gs/templates/chatWindow/item-card.hbs"
+        // And so on...
+    }
+
     preparedData(){
 
     }
@@ -6,6 +13,18 @@ export class GSItem extends Item {
 
     }
     async roll(){
+        let chatData = {
+            user: game.user._id,
+            speaker: ChatMessage.getSpeaker()
+        };
 
+        let cardData = {
+            ...this.data,
+            owner: this.actor.id
+        };
+
+        chatData.content = await renderTemplate(this.chatTemplate[this.type], cardData);
+        chatData.roll = true;
+        return ChatMessage.create(chatData);
     }
 }
